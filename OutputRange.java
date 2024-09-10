5d32ee86ef28359863e9ded1d1d6925bf91ac12b
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-10 13:51:01 -0400

    added while loop in if else; increments to value of firstInt, prints increment value followed by a space, increments firstInt by 5; print to format

--- OutputRange.java
+++ OutputRange.java
@@ -4,19 +4,23 @@ import java.util.Scanner;
 
 public class OutputRange {
    public static void main(String[] args) {
-      /* Type your code here. */
       Scanner scnr = new Scanner(System.in);
-      
       int firstInt = scnr.nextInt();
       int secondInt = scnr.nextInt();
+      int increments;
       
       if (secondInt < firstInt) {
-         System.out.println("Second integer can't be less than the first.");
+        System.out.println("Second integer can't be less than the first.");
       }
+
       else {
-         System.out.println("First Int: " + firstInt);
-         System.out.println("Second Int: " + secondInt);
-      
+        while(firstInt <= secondInt){
+            increments = firstInt;
+            System.out.print(increments + " ");
+            firstInt += 5;
+        }
+         System.out.println();
+         scnr.close();
       }
    }
 }
